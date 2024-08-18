7e9eb30a8ab82b49c13306b24d2d0e5989f5e6ed
ParikshitShetty <ParikshitShetty66@gmail.com>
2024-08-18 15:50:35 +0530

    selection sort implemented

---
+++ Sorting/Approach.md
@@ -0,0 +1,8 @@
+# Approach:
+
+- Find the smallest element in the array and exchange it with the element in the first position.
+- Find the second smallest element in the array and exchange it with the element in the second position.
+- Continue this process until done.
+
+### Time Complexity  : O(n2)
+### Space Complexity : O(1)
---
+++ Sorting/selectionSort.py
@@ -0,0 +1,15 @@
+def selectionSort(arr, size):
+    for i in range(size):
+        min_index = i
+        for j in range( i + 1, size):
+            # select the minimum element in every iteration
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
+size = len(arr)
+
+selectionSort( arr=arr, size=size )
+
+print(arr)
